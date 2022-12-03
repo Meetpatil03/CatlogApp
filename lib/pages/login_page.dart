3ad0e38a170d8e60d3ba909c76d8d9eb337fd53b
Meetpatil03 <meetpatil03@gmail.com>
2022-12-03 21:46:23 +0530

    Day4 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,13 +10,49 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
+            SizedBox(
+              height: 20.0,
+            ),
             Text(
               "Welcome",
               style: TextStyle(
-                fontSize: 22,
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
-            )
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "UserName",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter PassWord",
+                        labelText: "PassWord",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(),
+                      onPressed: () {
+                        print("Hi CatLog_app");
+                      },
+                    ),
+                  ],
+                ))
           ],
         ));
   }
